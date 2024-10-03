480b5014c493845fba78269f7fd26c470ba6b635
dkflfkd53 <b57062526@gmail.com>
2024-10-03 14:03:27 +0900

    first commit

---
+++ src/main/java/com/example/springoauthtest/SpringOauthTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springoauthtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauthTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springoauthtest/SpringOauthTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springoauthtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
